79d995770e2cd6445e456952d18c5b4f7f42b8a4
YamiDarknezz <erick.plasencia54@gmail.com>
2025-10-07 18:45:31 -0500

    feat(login): login funcional terminado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,16 @@
 import { Routes } from '@angular/router';
 import { Login } from './features/auth/login/login';
+import { Dashboard } from './features/dashboard/dashboard/dashboard';
 
 export const routes: Routes = [
   {
     path: '',
     redirectTo: 'login',
     pathMatch: 'full',
+  },
+  {
+    path: 'dashboard',
+    component: Dashboard,
   },
   {
     path: 'login',
--- src/app/features/auth/login/login.ts
+++ src/app/features/auth/login/login.ts
@@ -1,11 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { AuthService } from '../../../core/services/auth/auth';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule, CommonModule],
   templateUrl: './login.html',
-  styleUrl: './login.scss'
+  styleUrl: './login.scss',
 })
-export class Login {
+export class Login implements OnInit {
+  email = '';
+  password = '';
+  loading = false;
+  message = '';
+  errorMsg = '';
 
+  constructor(private auth: AuthService, private router: Router) {}
+
+  async ngOnInit() {
+    try {
+      const user = await this.auth.getUser();
+      if (user) {
+        console.log('🔄 Sesión detectada, redirigiendo al dashboard...');
+        await this.router.navigate(['/dashboard']);
+      }
+    } catch (error) {
+      console.warn('No hay sesión activa');
+    }
+  }
+
+  async onLogin() {
+    this.loading = true;
+    this.errorMsg = '';
+    this.message = '';
+
+    try {
+      const { user, message } = await this.auth.loginUser(
+        this.email,
+        this.password
+      );
+      this.message = message;
+      //console.log('Sesión activa:', user); Debug
+
+      // Redirigir al dashboard después del login exitoso
+      await this.router.navigate(['/dashboard']);
+    } catch (error: any) {
+      this.errorMsg = error.message || 'Error al iniciar sesión';
+      console.error('Error en login:', error);
+    } finally {
+      this.loading = false;
+    }
+  }
 }
