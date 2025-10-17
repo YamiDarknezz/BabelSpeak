0952e57fd4b394b8f5afb02ed64cba6b644fed60
YamiDarknezz <erick.plasencia54@gmail.com>
2025-10-17 17:37:18 -0500

    feat: responsividad en el sidebar

--- src/app/features/dashboard/sidebar/sidebar.ts
+++ src/app/features/dashboard/sidebar/sidebar.ts
@@ -1,18 +1,37 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  Input,
+  Output,
+  EventEmitter,
+  HostBinding,
+} from '@angular/core';
 import { AuthService } from '../../../core/services/auth/auth';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, CommonModule],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.scss',
 })
 export class Sidebar {
+  @Input() isOpen = false;
+  @Output() closeRequested = new EventEmitter<void>();
+
+  @HostBinding('class.sidebar-open') get sidebarOpenClass() {
+    return this.isOpen;
+  }
+
   constructor(private auth: AuthService, private router: Router) {}
 
   async logout() {
     await this.auth.logout();
     await this.router.navigate(['/login']);
+  }
+
+  onLinkClick() {
+    // Cerrar sidebar en mobile cuando se hace clic en un link
+    this.closeRequested.emit();
   }
 }
